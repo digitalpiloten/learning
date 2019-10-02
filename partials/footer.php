cfa3c56e39977593c4ba4cd0b251b62445265ca1
MagicMarvMan <marvin.kai@gmx.de>
2019-10-02 17:23:58 +0200

    Add scripts to footer partial

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,5 @@
 
+        <script type="application/javascript" src="https://libs.digitalpiloten.org/jquery/3.4.1/jquery.min.js"></script>
+        <script type="application/javascript" src="<?= $bootstrap_js ?>"></script>
     </body>
 </html>
