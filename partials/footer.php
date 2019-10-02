db5b27f622a51d3045b5346aaf88f0e3c7df38ad
MagicMarvMan <marvin.kai@gmx.de>
2019-10-02 19:01:05 +0200

    Add number of database queries to footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,4 +1,10 @@
 
+            <footer>
+                <br><hr>
+                <p>
+                    <?= $database->getQueryNum() ?> Queries.
+                </p>
+            </footer>
         </div>
         <script type="application/javascript" src="https://libs.digitalpiloten.org/jquery/3.4.1/jquery.min.js"></script>
         <script type="application/javascript" src="<?= $bootstrap_js ?>"></script>
