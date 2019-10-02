7d13f5b6719f67eae9f27bbb09394bf341388679
MagicMarvMan <marvin.kai@gmx.de>
2019-10-02 18:58:40 +0200

    Improve database class

--- lib/DatabaseManager.inc.php
+++ lib/DatabaseManager.inc.php
@@ -7,6 +7,7 @@ class DatabaseManager {
     private $username = "";
     private $password = "";
     private $pdo;
+    private $queryNum = 0;
 
     function __construct($host, $database, $username, $password) {
         $this->host = $host;
@@ -17,5 +18,14 @@ class DatabaseManager {
 
     public function connect() {
         $this->pdo = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database, $this->username, $this->password);
+    }
+
+    public function query($sql) {
+        $queryNum++;
+        return($this->pdo->query($sql));
+    }
+
+    public function getQueryNum() {
+        return($this->queryNum);
     }
 }
